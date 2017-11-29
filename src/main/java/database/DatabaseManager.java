d941ec238dd8cfeacdb6d0b10f1541857083da8b
Washigeri <malek.elouerghi@gmail.com>
2017-11-29 17:25:45 -0500

    test

--- src/main/java/database/DatabaseManager.java
+++ src/main/java/database/DatabaseManager.java
@@ -1,4 +1,29 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseManager {
+
+    private static String path = "src/main/resources/";
+
+    public static void CreateDatabase(String filename){
+        String url = "jdbc:sqlite:" + path + filename;
+        try{
+            Connection connection  = DriverManager.getConnection(url);
+            if(connection != null){
+                DatabaseMetaData meta = connection.getMetaData();
+                System.out.println(meta.getDriverName());
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        CreateDatabase("test.db");
+    }
 }
