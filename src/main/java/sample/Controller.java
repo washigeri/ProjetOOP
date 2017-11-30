7e9dfdd503eef09381f694064c204beac49dcc7f
BarjonP <pierre.barjon225@gmail.com>
2017-11-30 16:42:09 -0500

    fonction premier onglet

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,5 +1,21 @@
 package sample;
 
+import java.util.Date;
+import java.util.List;
+
+import database.DatabaseManager;
+import database.IDatabaseManager;
+import models.*;
 
 public class Controller {
+	
+	private IDatabaseManager databaseManager = DatabaseManager.getInstance();
+	
+	public float GetSpendingsOverLastXDays(int numberOfDays)
+	{
+		Date now = new Date();
+		List<Transaction> allTransactions = databaseManager.SelectAll(Transaction.class);
+	}
+	
+	
 }
