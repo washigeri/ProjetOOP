fe13d3c8dc6d0f1ad34811bbf2bad96a53a4a0a5
Paul Baril <paulbaril1995@hotmail.fr>
2017-12-01 00:48:30 -0500

    Modif scene et Controller pour les boutons et recuperer les infos du formulaire

--- src/main/java/sample/AddTransactionController.java
+++ src/main/java/sample/AddTransactionController.java
@@ -3,32 +3,103 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import database.DatabaseManager;
+import models.Category;
+import models.Spending;
+import models.Transaction;
+import models.User;
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+
+import java.sql.SQLException;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class AddTransactionController {
-		
-    @FXML
-	private TextField TextField_AddTransaction_Montant; 
-    
-    @FXML
-    private TextField TextField_AddTransaction_Repetition;
-    
-    @FXML
-    private TextArea TextArea_AddTransaction_Description;
-    
-    @FXML
-    private Button Button_AddTransaction_AddTransaction;
-    
-    @FXML
-    private Button Button_AddTransaction_AddCategorie;
-    
-    @FXML
-    private ChoiceBox<String> ChoiceBox_AddTransaction_Categorie;
-    
-    
+
+	private static DatabaseManager databaseManager = DatabaseManager.getInstance();
+
+	@FXML
+	public TextField TextField_AddTransaction_Montant;
+
+	@FXML
+	public TextField TextField_AddTransaction_Repetition;
+
+	@FXML
+	public TextArea TextArea_AddTransaction_Description;
+
+	@FXML
+	public Button Button_AddTransaction_AddTransaction;
+
+	@FXML
+	public Button Button_AddTransaction_AddCategorie;
+
+	@FXML
+	public ChoiceBox<String> ChoiceBox_AddTransaction_Categorie;
+
+	@FXML
+	public ChoiceBox<String> ChoiceBox_AddTransaction_Repetition;
+
+	@FXML
+	public DatePicker DatePicker_AddTransaction_Start;
+
+	@FXML
+	public DatePicker DatePicker_AddTransaction_End;
+
 	public AddTransactionController() {
-		
+
+	}
+
+	@FXML
+	private void handleAddTransactionButtonAction() {
+		// Button was clicked, do something...
+		System.out.println("Button AddTransaction Clicked");
+		System.out.println(TextField_AddTransaction_Montant.getText());
+		System.out.println(TextField_AddTransaction_Repetition.getText());
+	}
+
+	@FXML
+	private void handleAddCategoryButtonAction() {
+		// Button was clicked, do something...
+		System.out.println("Button AddCategory Clicked");
+
+	}
+
+	private static void CreateNewSpending(float amount, Category category, Date date, String description,
+			Transaction transaction) throws SQLException {
+		int availableID = databaseManager.GetLastID(Spending.class);
+		Spending spending = new Spending(availableID, amount, description, date, category, transaction);
+		databaseManager.Insert(spending);
+	}
+
+	public static boolean CreateNewTransaction(float amount, Category category, int frequency, String description,
+			User user, Date startDate, Date endDate) {
+		try {
+			int availableID = databaseManager.GetLastID(Transaction.class);
+			Transaction transaction = new Transaction(availableID, user, description, amount, new Date(), startDate,
+					endDate, frequency, category);
+			databaseManager.Insert(transaction);
+			if (frequency > 0) {
+				int inBetweenDays = Days.daysBetween(new DateTime(startDate), new DateTime(endDate)).getDays();
+				DateTime startDateJ = new DateTime(startDate);
+				for (int i = 0; i < inBetweenDays; i += frequency) {
+					DateTime newDate = startDateJ.plusDays(i * frequency);
+					CreateNewSpending(transaction.getAmount(), transaction.getCategory(), newDate.toDate(),
+							transaction.getDescription(), transaction);
+				}
+				return true;
+			} else {
+				CreateNewSpending(transaction.getAmount(), transaction.getCategory(), transaction.getCreationDate(),
+						transaction.getDescription(), transaction);
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 }
-
